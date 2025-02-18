ffbe4f249f7cdbd7294f0d42692c5d69ae8ce2d6
Prasadrasal2002 <145026242+Prasadrasal2002@users.noreply.github.com>
2025-02-18 11:11:06 +0530

    Create SpringBootEKSApplication.java

---
+++ src/main/java/com/example/demo/SpringBootEKSApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEKSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEKSApplication.class, args);
+    }
+}
